8c3b0b8fe60eba739f765582b894034c0e7ca961
Jhon Raymond <horse2019@protonmail.com>
2020-02-06 16:20:41 +0800

    Updates

---
+++ precache-manifest.a68f24e1526c1cfa8eaa3bc215bfbb9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c5245e79225896a855f92e33f0c5c2",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c1f34d516d4df64a6597",
+    "url": "/react-portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e7c6f97adfdf1eab0f8d",
+    "url": "/react-portfolio/static/js/2.b4f80e79.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-portfolio/static/js/2.b4f80e79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f34d516d4df64a6597",
+    "url": "/react-portfolio/static/js/main.a9b6538f.chunk.js"
+  },
+  {
+    "revision": "2827de04ebfd7bdc4759",
+    "url": "/react-portfolio/static/js/runtime-main.85db4478.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
