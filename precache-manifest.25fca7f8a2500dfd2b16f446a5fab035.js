393c7579c4e88c7b00136abfbed7da6f6a5e9937
Jhon Raymond <horse2019@protonmail.com>
2020-02-09 00:29:16 +0800

    Updates

---
+++ precache-manifest.25fca7f8a2500dfd2b16f446a5fab035.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf27170eaeb577b33c1652e9c30ed74a",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c81e1c24f51f934e64e9",
+    "url": "/react-portfolio/static/css/main.b0db670a.chunk.css"
+  },
+  {
+    "revision": "0b7774c4579ccda6feaa",
+    "url": "/react-portfolio/static/js/2.b0b8c8ab.chunk.js"
+  },
+  {
+    "revision": "2bcf73e3839e96bb488d3fc4da7ab52a",
+    "url": "/react-portfolio/static/js/2.b0b8c8ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c81e1c24f51f934e64e9",
+    "url": "/react-portfolio/static/js/main.3aabc8b1.chunk.js"
+  },
+  {
+    "revision": "2827de04ebfd7bdc4759",
+    "url": "/react-portfolio/static/js/runtime-main.85db4478.js"
+  },
+  {
+    "revision": "5006b474658013304f9c32f5268f8d00",
+    "url": "/react-portfolio/static/media/jhon.5006b474.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.bc87665942903c80ae7eb02ed32ec9e5.js"
+  "/react-portfolio/precache-manifest.25fca7f8a2500dfd2b16f446a5fab035.js"
 );
 
 self.addEventListener('message', (event) => {
