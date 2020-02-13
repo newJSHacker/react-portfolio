7c035ab7555ed3b6312525c54dc2da7510c188cc
Jhon Raymond <horse2019@protonmail.com>
2020-02-13 18:19:50 +0800

    Site Modification

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './contents/Home'
-import About from './contents/About'
-import Education from './contents/Education'
-import Skills from './contents/Skills'
-import Contact from './contents/Contact'
+import React, {useState, useEffect} from 'react'
+import Pages from './pages'
 
 function App(){
-    return(
-        <Router>
-            <div className="App">
-                <Navbar />
-                <Route exact path="/react-portfolio">
-                    <Home />
-                </Route>
-                <Route path="/react-portfolio/about">
-                    <About />
-                </Route>
-                <Route path="/react-portfolio/education">
-                    <Education/>
-                </Route>
-                <Route path="/react-portfolio/skills">
-                    <Skills/>
-                </Route>
-                <Route path="/react-portfolio/contact">
-                    <Contact />
-                </Route>
-            </div>
-        </Router>
-    )
+    const [user, setUser] = useState(null)
+    useEffect(() => {
+        fetch('https://gitconnected.com/v1/portfolio/treyhuffine')
+            .then(res => res.json())
+            .then(user => {
+                setUser(user)
+            })
+    }, [])
+
+    if(!user) {
+        return <div />
+    }
+
+    return <Pages user={user} />
 }
 
 export default App
